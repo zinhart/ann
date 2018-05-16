415126e677a93357408fb1d5921cb3e2ef09a9ad
zinhart <azdegrac@ucsc.edu>
2018-05-16 12:33:54 -0700

    90% with unit test for forward_propagate

--- test/ann_test.cc
+++ test/ann_test.cc
@@ -73,6 +73,7 @@ TEST(ann_test, forward_propagate)
   std::random_device rd;
   std::mt19937 mt(rd());
   std::uniform_int_distribution<std::uint32_t> dist(0,5000);// causes a bad alloc when appro > when a 3 layer model has > 5000 neurons in each //layer machine limitations :(
+  std::uniform_real_distribution<float> real_dist(std::numeric_limits<float>::min(), std::numeric_limits<float>::max() );
   std::uint32_t total_num_targets = dist(mt);
 
   ann< ffn > model;
@@ -86,55 +87,134 @@ TEST(ann_test, forward_propagate)
   a_layer.first = ACTIVATION_NAME::RELU;
   a_layer.second = total_num_targets; 
   add_layer(model,a_layer);
-  
+
+  // host vectors 
   std::pair<std::uint32_t, std::shared_ptr<double>> total_observations;
   std::pair<std::uint32_t, std::shared_ptr<double>> total_targets;
-  std::vector<LAYER_INFO> total_layers(get_total_layers(model));
-  std::uint32_t total_hidden_weights, total_activations, ith_layer, total_bias;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_activations;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_bias;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_hidden_weights;
+
+  // host validation vectors
   
-  total_observations.first = dist(mt);//number of observations
-  total_observations.second = std::shared_ptr<double> ( new double[total_observations.first * total_layers[0].second], std::default_delete<double[]>() );//observations themselves 
-  total_targets.first = total_num_targets; // number of targets
-  total_targets.second = std::shared_ptr<double> ( new double[total_targets.first], std::default_delete<double[]>() );//targets themselves 
-    
-  //calc number of activations
-  for(ith_layer = 1, total_activations = 0; ith_layer < total_layers.size(); ++ith_layer )
-	total_activations += total_layers[ith_layer].second;//accumulate neurons in the hidden layers and output layer
-  
-  //calc number of hidden weights
-  for(ith_layer = 0, total_hidden_weights = 0; ith_layer < total_layers.size() - 1; ++ith_layer)
-  {
-	total_hidden_weights += total_layers[ith_layer + 1].second * total_layers[ith_layer].second; 
-  }  
-  total_bias = total_layers.size() - 1;
-  cudaError_t error_id;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_observations_copy;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_targets_copy;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_activations_copy;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_bias_copy;
+  std::pair<std::uint32_t, std::shared_ptr<double>> total_hidden_weights_copy;
+
   // device vectors
   double * device_total_observations, * device_total_activations, * device_total_bias, * device_total_hidden_weights;
 
-  error_id = cudaMalloc( (void **) &device_total_observations, total_observations.first * sizeof(double) );
+
+  std::vector<LAYER_INFO> total_layers(get_total_layers(model));
+  std::uint32_t  i, ith_layer, total_observations_length;
+  total_observations_length = total_observations.first * total_layers[0].second;
+  
+  total_observations.first = dist(mt);//number of observations
+  total_observations.second = std::shared_ptr<double> ( new double[total_observations_length], std::default_delete<double[]>() );//observations themselves 
+  total_targets.first = total_num_targets; // number of targets
+  total_targets.second = std::shared_ptr<double> ( new double[total_targets.first], std::default_delete<double[]>() );//targets themselves 
+    
+  // calc number of activations
+  for(ith_layer = 1, total_activations.first = 0; ith_layer < total_layers.size(); ++ith_layer )
+	total_activations.first += total_layers[ith_layer].second;// accumulate neurons in the hidden layers and output layer
+  
+  // calc number of hidden weights
+  for(ith_layer = 0, total_hidden_weights.first = 0; ith_layer < total_layers.size() - 1; ++ith_layer)
+	total_hidden_weights.first += total_layers[ith_layer + 1].second * total_layers[ith_layer].second;
+  
+  // allocate host activation and weight activations
+  total_activations.second = std::shared_ptr<double> ( new double[total_activations.first], std::default_delete<double[]>() );
+  total_hidden_weights.second = std::shared_ptr<double> ( new double[total_hidden_weights.first], std::default_delete<double[]>() );
+
+  // calc bias neurons
+  total_bias.first = total_layers.size() - 1;
+  total_bias.second = std::shared_ptr<double> ( new double[total_bias.first], std::default_delete<double[]>() );
+  cudaError_t error_id;
+
+  // deal with validation vectors
+  total_observations_copy.first = total_observations.first;//number of observations
+  total_observations_copy.second = std::shared_ptr<double> ( new double[total_observations_length], std::default_delete<double[]>() );//observations themselves 
+  total_targets_copy.first = total_num_targets; // number of targets
+  total_targets_copy.second = std::shared_ptr<double> ( new double[total_targets.first], std::default_delete<double[]>() );//targets themselves 
+  total_activations_copy.first = total_activations.first;
+  total_activations_copy.second = std::shared_ptr<double> ( new double[total_activations_copy.first], std::default_delete<double[]>() );
+  total_bias_copy.first = total_bias.first;
+  total_bias_copy.second = std::shared_ptr<double> ( new double[total_bias_copy.first], std::default_delete<double[]>() );
+  total_hidden_weights_copy.first = total_hidden_weights.first;
+  total_hidden_weights_copy.second = std::shared_ptr<double> ( new double[total_hidden_weights_copy.first], std::default_delete<double[]>() );
+
+  // extra caution because if these assertions aren't true this test case breaks
+  ASSERT_EQ(total_observations.first, total_observations_copy.first);
+  ASSERT_EQ(total_targets.first, total_targets_copy.first);
+  ASSERT_EQ(total_activations.first, total_activations_copy.first);
+  ASSERT_EQ(total_bias.first, total_bias_copy.first);
+  ASSERT_EQ(total_hidden_weights.first, total_hidden_weights_copy.first);
+   
+  // initialize host vectors with dummy data  
+  for(i = 0; i < total_observations_length; ++i)
+	total_observations.second.get()[i] = real_dist(mt); // random training set
+  for(i = 0; i < total_activations.first; ++i)
+	total_activations.second.get()[i] = 0.0; // start at zero since these values have not been calculated yet
+  for(i = 0; i < total_hidden_weights.first; ++i)
+	total_hidden_weights.second.get()[i] = real_dist(mt); // random weights
+  for(i = 0; i < total_bias.first; ++i)
+	total_bias.second.get()[i] = real_dist(mt); // random bias (which is an oxymoron?)
+
+  // allocate device vectors
+  error_id = cudaMalloc( (void **) &device_total_observations, total_observations_length * sizeof(double) );
   if(error_id != cudaSuccess)
 	std::cerr<<"device_total_observations allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
 
-  error_id = cudaMalloc( (void **) &device_total_activations, total_activations * sizeof(double) );
+  error_id = cudaMalloc( (void **) &device_total_activations, total_activations.first * sizeof(double) );
   if(error_id != cudaSuccess)
 	std::cerr<<"device_total_activations allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
   
-  error_id = cudaMalloc( (void **) &device_total_bias, total_bias * sizeof(double) );
+  error_id = cudaMalloc( (void **) &device_total_bias, total_bias.first * sizeof(double) );
   if(error_id != cudaSuccess)
 	std::cerr<<"device_total_bias allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
 	
-  error_id = cudaMalloc( (void **) &device_total_hidden_weights, total_hidden_weights * sizeof(double) );
+  error_id = cudaMalloc( (void **) &device_total_hidden_weights, total_hidden_weights.first * sizeof(double) );
   if(error_id != cudaSuccess)
 	std::cerr<<"device_total_hidden_weights allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
 
-
-
+  // copy host memory to device
+  error_id = cudaMemcpy(device_total_observations, total_observations.second.get(), total_observations_length * sizeof(double), cudaMemcpyHostToDevice);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_total_observations (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  error_id = cudaMemcpy(device_total_activations, total_activations.second.get(), total_activations.first * sizeof(double), cudaMemcpyHostToDevice);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_total_activations (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  error_id = cudaMemcpy(device_total_bias, total_bias.second.get(), total_bias.first *  sizeof(double), cudaMemcpyHostToDevice);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_total_bias (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  error_id = cudaMemcpy(device_total_hidden_weights, total_hidden_weights.second.get(), total_hidden_weights.first * sizeof(double), cudaMemcpyHostToDevice);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_total_hidden_weights (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
 
   //ASSERT_EQ(initialize_model(model, total_observations, total_targets), 0);
   //ASSERT_EQ(total_activations, get_total_activations(model).first);
   //ASSERT_EQ(total_hidden_weights, get_total_hidden_weights(model).first);
   //call forward_propagate wrapper here
   //ASSERT_EQ(cleanup(model), 0);
+  
+  // copy device memory back to host
+  error_id = cudaMemcpy( total_observations_copy.second.get(), device_total_observations, total_observations_length * sizeof(double), cudaMemcpyDeviceToHost);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_observation (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  error_id = cudaMemcpy( total_activations_copy.second.get(), device_total_activations, total_activations.first * sizeof(double), cudaMemcpyDeviceToHost);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_activations (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  error_id = cudaMemcpy( total_bias_copy.second.get(), device_total_bias, total_bias.first * sizeof(double), cudaMemcpyDeviceToHost);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_bias (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  error_id = cudaMemcpy( total_hidden_weights_copy.second.get(), device_total_hidden_weights, total_hidden_weights.first * sizeof(double), cudaMemcpyDeviceToHost);
+  if(error_id != cudaSuccess)
+	std::cerr<<"device_total_hidden_weights (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+
+
+  // validate correctness, the only matrix that should not be the same is device activations
   
   // deallocate device memory and check for errors  
   error_id = cudaFree(device_total_observations);
@@ -149,8 +229,6 @@ TEST(ann_test, forward_propagate)
   error_id = cudaFree(device_total_hidden_weights);
   if(error_id != cudaSuccess)
 	std::cerr<<"device_total_hidden_weights deallocation (In forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-
-
 }
 
 TEST(ann_test, ann_train)
