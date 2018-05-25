ec579f18cc159ab18bd51634f5a4bef195467aa3
zinhart <azdegrac@ucsc.edu>
2018-05-24 18:50:48 -0700

    Added and destroyed cuda streams successfully, passed memcheck

--- test/ann_test.cc
+++ test/ann_test.cc
@@ -72,6 +72,7 @@ TEST(ann_test, initialize_model)
 TEST(ann_test, forward_propagate)
 {
   std::uint32_t total_pinned_memory = 0;
+  const std::uint32_t n_cuda_streams = MAX_CPU_THREADS;
   // set device properties
   cudaDeviceProp properties;
   zinhart::check_cuda_api(cudaGetDeviceProperties(&properties,0),__FILE__, __LINE__);
@@ -82,8 +83,10 @@ TEST(ann_test, forward_propagate)
   std::uniform_real_distribution<float> real_dist(std::numeric_limits<float>::min(), std::numeric_limits<float>::max() );
   std::uniform_int_distribution<std::uint8_t> layer_num_dist(1,10/*std::numeric_limits<std::uint8_t>::max()*/);
   std::uniform_int_distribution<std::uint8_t> activation_dist(1,8);// currently there are 8 different activation functions not counting the input layer
+  //std::uniform_int_distribution<std::uint8_t> cuda_stream_dist(1, MAX_CPU_THREADS); 
   std::uint32_t total_num_targets = neuron_dist(mt);
   std::vector<LAYER_INFO> total_layers(layer_num_dist(mt));
+  cudaStream_t * streams; 
 
   // host vectors 
   std::pair<std::uint32_t, double *> total_observations;
@@ -121,7 +124,6 @@ TEST(ann_test, forward_propagate)
   ASSERT_EQ(total_layers[0].first, ACTIVATION_NAME::INPUT);
 
   // calculate array sizes
-  
   total_observations.first = neuron_dist(mt);// number of observations
   total_targets.first = total_num_targets; // number of targets
   total_observations_length = total_observations.first * total_layers[0].second;
@@ -170,8 +172,17 @@ TEST(ann_test, forward_propagate)
   ASSERT_EQ(0,zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_activations, total_activations.first * sizeof(double) ) ,__FILE__,__LINE__ ));
   ASSERT_EQ(0,zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_bias, total_bias.first * sizeof(double) ) ,__FILE__,__LINE__));
   ASSERT_EQ(0,zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_hidden_weights, total_hidden_weights.first * sizeof(double) ) ,__FILE__,__LINE__));
+
+  // allocate streams 
+  streams = new cudaStream_t[n_cuda_streams];
+
+  // create streams
+  for (i = 0; i < n_cuda_streams; ++i)
+  {
+	ASSERT_EQ(0, zinhart::check_cuda_api(cudaStreamCreate(&streams[i]),__FILE__,__LINE__));
+  }
+
   // initialize host vectors
-  
   for(i = 0; i < total_observations_length; ++i)
 	total_observations.second[i] = real_dist(mt); // random training set
     for(i = 0; i < total_activations.first; ++i)
@@ -181,185 +192,18 @@ TEST(ann_test, forward_propagate)
   for(i = 0; i < total_bias.first; ++i)
 	total_bias.second[i] = real_dist(mt); // random bias (which is an oxymoron?)
 
- //ASSERT_EQ(0,zinhart::check_cuda_api(cudaMemcpy(device_total_observations, total_observations.second, total_observations_length * sizeof(double), cudaMemcpyHostToDevice))); 
-
-  // copy host memory to device
-/*  error_id = cudaMemcpy(device_total_observations, total_observations.second.get(), total_observations_length * sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_observations (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy(device_total_activations, total_activations.second.get(), total_activations.first * sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_activations (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy(device_total_bias, total_bias.second.get(), total_bias.first *  sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_bias (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy(device_total_hidden_weights, total_hidden_weights.second.get(), total_hidden_weights.first * sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_hidden_weights (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";*/
-
-/*  total_observations.second = std::shared_ptr<double> ( new double[total_observations_length], std::default_delete<double[]>() );//observations themselves 
-
-  // calc bias neurons
-  total_bias.first = total_layers.size() - 1;
-
-  // allocate host vectors
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_observations.second, sizeof(double) * total_observations.first, cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_targets.second, sizeof(double) * total_targets.first, cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_activations.second, sizeof(double) * total_activations.first, cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_hidden_weights.second, sizeof(double) * total_hidden_weights.first,cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_bias.second, sizeof(double) * total_bias.first,cudaHostAllocDefault),__FILE__,__LINE__);
-
-  // allocate validation vectors
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_observations_copy, sizeof(double) * total_observations.first, cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_targets_copy, sizeof(double) * total_targets.first, cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_activations_copy, sizeof(double) * total_activations.first, cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_hidden_weights_copy, sizeof(double) * total_hidden_weights.first,cudaHostAllocDefault),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaHostAlloc((void**)&total_bias_copy, sizeof(double) * total_bias.first,cudaHostAllocDefault),__FILE__,__LINE__);
-
-
-  // allocate device vectors
-  zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_observations, total_observations_length * sizeof(double) ),__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_activations, total_activations.first * sizeof(double) ) ,__FILE__,__LINE__ );
-  zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_bias, total_bias.first * sizeof(double) ) ,__FILE__,__LINE__);
-  zinhart::check_cuda_api(cudaMalloc( (void **) &device_total_hidden_weights, total_hidden_weights.first * sizeof(double) ) ,__FILE__,__LINE__);
-
-  // initialize host vectors
-  //
-  //
-  total_observations.second = std::shared_ptr<double> ( new double[total_observations_length], std::default_delete<double[]>() );//observations themselves 
-    total_targets.second = std::shared_ptr<double> ( new double[total_targets.first], std::default_delete<double[]>() );//targets themselves 
+  //ASSERT_EQ(0,zinhart::check_cuda_api(cudaMemcpy(device_total_observations, total_observations.second, total_observations_length * sizeof(double), cudaMemcpyHostToDevice))); 
   
-  
-  // allocate host activation and weight activations
-  total_activations.second = std::shared_ptr<double> ( new double[total_activations.first], std::default_delete<double[]>() );
-  total_hidden_weights.second = std::shared_ptr<double> ( new double[total_hidden_weights.first], std::default_delete<double[]>() );
+  // copy host memory to device 
 
-    total_bias.second = std::shared_ptr<double> ( new double[total_bias.first], std::default_delete<double[]>() );
-  cudaError_t error_id;
-
-  // deal with validation vectors
-  total_observations_copy.first = total_observations.first;//number of observations
-  total_observations_copy.second = std::shared_ptr<double> ( new double[total_observations_length], std::default_delete<double[]>() );//observations themselves 
-  total_targets_copy.first = total_num_targets; // number of targets
-  total_targets_copy.second = std::shared_ptr<double> ( new double[total_targets.first], std::default_delete<double[]>() );//targets themselves 
-  total_activations_copy.first = total_activations.first;
-  total_activations_copy.second = std::shared_ptr<double> ( new double[total_activations_copy.first], std::default_delete<double[]>() );
-  total_bias_copy.first = total_bias.first;
-  total_bias_copy.second = std::shared_ptr<double> ( new double[total_bias_copy.first], std::default_delete<double[]>() );
-  total_hidden_weights_copy.first = total_hidden_weights.first;
-  total_hidden_weights_copy.second = std::shared_ptr<double> ( new double[total_hidden_weights_copy.first], std::default_delete<double[]>() );
-
-  // extra caution because if these assertions aren't true this test case breaks
-  ASSERT_EQ(total_observations.first, total_observations_copy.first);
-  ASSERT_EQ(total_targets.first, total_targets_copy.first);
-  ASSERT_EQ(total_activations.first, total_activations_copy.first);
-  ASSERT_EQ(total_bias.first, total_bias_copy.first);
-  ASSERT_EQ(total_hidden_weights.first, total_hidden_weights_copy.first);
-   
-  // initialize host vectors with dummy data  
-  for(i = 0; i < total_observations_length; ++i)
-	total_observations.second.get()[i] = real_dist(mt); // random training set
-  for(i = 0; i < total_activations.first; ++i)
-	total_activations.second.get()[i] = 0.0; // start at zero since these values have not been calculated yet
-  for(i = 0; i < total_hidden_weights.first; ++i)
-	total_hidden_weights.second.get()[i] = real_dist(mt); // random weights
-  for(i = 0; i < total_bias.first; ++i)
-	total_bias.second.get()[i] = real_dist(mt); // random bias (which is an oxymoron?)
-
-  // allocate device vectors
-  error_id = cudaMalloc( (void **) &device_total_observations, total_observations_length * sizeof(double) );
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_observations allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-
-  error_id = cudaMalloc( (void **) &device_total_activations, total_activations.first * sizeof(double) );
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_activations allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  
-  error_id = cudaMalloc( (void **) &device_total_bias, total_bias.first * sizeof(double) );
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_bias allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-	
-  error_id = cudaMalloc( (void **) &device_total_hidden_weights, total_hidden_weights.first * sizeof(double) );
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_hidden_weights allocation (in forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-
-  // copy host memory to device
-  error_id = cudaMemcpy(device_total_observations, total_observations.second.get(), total_observations_length * sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_observations (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy(device_total_activations, total_activations.second.get(), total_activations.first * sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_activations (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy(device_total_bias, total_bias.second.get(), total_bias.first *  sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_bias (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy(device_total_hidden_weights, total_hidden_weights.second.get(), total_hidden_weights.first * sizeof(double), cudaMemcpyHostToDevice);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_hidden_weights (HostToDevice) memcpy failed with error: "<<cudaGetErrorString(error_id)<<"\n"; */
-
-/*
- // cublas initialization and error check
-  cublasStatus_t cublas_error_id;
-  cublasHandle_t handle;
-  cublas_error_id = cublasCreate(&handle);
-  if(cublas_error_id != CUBLAS_STATUS_SUCCESS)
-  {
-	std::cerr<<"CublasHandle creation failed with error: "<<cublasGetErrorString(cublas_error_id)<<"\n";
-  }
-
-  // This is the forward propagation loop that is to be validated
-  // The only matrix that should not be the same is device activations
-    
-
-  for(ith_observation = 0; ith_observation < total_observations.first / total_observations.first; ++ith_observation)
-  {
-	error = forward_propagate(model, false, handle, ith_observation, total_layers, total_targets, total_hidden_weights, total_activations, device_total_observations, device_total_activations, device_total_bias, device_total_hidden_weights);
-
-	if(error == 1)
-	{
-	  std::cerr<<"An error occured in forward_propagate during the "<<ith_observation<<"th iterator\n";
-	  std::abort();
-	}
-  }
-  
+  // cublas initialization and error check
+ 
   // release cublas resources and check for errors
-  cublas_error_id = cublasDestroy(handle);
-  if(cublas_error_id != CUBLAS_STATUS_SUCCESS)
-  {
-	std::cerr<<"cublas handle destruction failed with error: "<<cublasGetErrorString(cublas_error_id)<<"\n";
-  }
   
   // copy device memory back to host
-  error_id = cudaMemcpy( total_observations_copy.second.get(), device_total_observations, total_observations_length * sizeof(double), cudaMemcpyDeviceToHost);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_observation (DeviceToHost) copy failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy( total_activations_copy.second.get(), device_total_activations, total_activations.first * sizeof(double), cudaMemcpyDeviceToHost);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_activations (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy( total_bias_copy.second.get(), device_total_bias, total_bias.first * sizeof(double), cudaMemcpyDeviceToHost);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_bias (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaMemcpy( total_hidden_weights_copy.second.get(), device_total_hidden_weights, total_hidden_weights.first * sizeof(double), cudaMemcpyDeviceToHost);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_hidden_weights (DeviceToHost) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
+  
+  // validate  
 
-  // deallocate device memory and check for errors  
-  zinhart::check_cuda_api(cudaFree(device_total_observations), __FILE__, __LINE__);
-  zinhart::check_cuda_api(cudaFree(device_total_activations), __FILE__, __LINE__);
-  zinhart::check_cuda_api(cudaFree(device_total_bias), __FILE__, __LINE__);
-  zinhart::check_cuda_api(cudaFree(device_total_hidden_weights), __FILE__, __LINE__);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_observations deallocation (In forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaFree(device_total_activations);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_activations deallocation (In forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaFree(device_total_bias);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_bias deallocation (In forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-  error_id = cudaFree(device_total_hidden_weights);
-  if(error_id != cudaSuccess)
-	std::cerr<<"device_total_hidden_weights deallocation (In forward_propagate) failed with error: "<<cudaGetErrorString(error_id)<<"\n";
-*/
   // deallocate host memory and check for errors
   ASSERT_EQ(0, zinhart::check_cuda_api(cudaFreeHost(total_observations.second),__FILE__,__LINE__));
   ASSERT_EQ(0, zinhart::check_cuda_api(cudaFreeHost(total_targets.second),__FILE__,__LINE__));
@@ -382,6 +226,15 @@ TEST(ann_test, forward_propagate)
   ASSERT_EQ(0, zinhart::check_cuda_api(cudaFree(device_total_activations), __FILE__, __LINE__));
   ASSERT_EQ(0, zinhart::check_cuda_api(cudaFree(device_total_bias), __FILE__, __LINE__));
   ASSERT_EQ(0, zinhart::check_cuda_api(cudaFree(device_total_hidden_weights), __FILE__, __LINE__));
+
+  // destroy cuda streams
+   for (i = 0; i < n_cuda_streams; ++i)
+  {
+	ASSERT_EQ(0, zinhart::check_cuda_api(cudaStreamDestroy(streams[i]),__FILE__,__LINE__));
+  }
+
+  // deallocate cuda streams
+  delete [] streams;
 
   ASSERT_EQ(0,zinhart::check_cuda_api(cudaDeviceReset(), __FILE__, __LINE__));
 }
