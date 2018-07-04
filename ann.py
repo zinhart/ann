5fe8e0a91c4c75f244ff7d41e6dd4f75772b5ea6
zinhart <azdegrac@ucsc.edu>
2018-07-04 14:47:13 -0700

    Finished menu recursion loop

--- ann.py
+++ ann.py
@@ -49,16 +49,19 @@ ann_image_build_cmds = [
                         "docker build",
                         "nvidia-docker build -t ann ."
                        ]
-def build_cpu_image():
-    #recursive_menu_selection()
+def build_cpu_image(menu_obj):
     output_bytes = client.build(path = "./", tag = "ann")
     for item in output_bytes:
         pretty_output = json.loads(item)
         pretty_output = pretty_output['stream']
         print(pretty_output, end='')
-    input("Press [Enter] to continue")
+    display_menu_options(menu_obj)
+    get_sub_menu_selection(menu_obj)
+    #next_menu_selection(menu_obj)
+    #input("Press [Enter] to continue")
 
-def enter_cpu_image():
+def enter_cpu_container(menu_obj):
+    print("In enter cpu container")
     return;
 
 #have to set this to use nvidia-docker via the client runtime parameter (something like that)
@@ -68,43 +71,82 @@ def build_gpu_image():
         pretty_output = json.loads(item)
         pretty_output = pretty_output['stream']
         print(pretty_output, end='')
-    #input("Press [Enter] to continue")
+    input("Press [Enter] to continue")
 
-def enter_gpu_image():
+def enter_gpu_container():
     return;
 
 
-def back_to_base_menu():
-    print (ann_base_menu_choices)
+def get_main_menu_selection(menu_obj):
+    choice = input(">> ")
+    # check for errors
+    try:
+        if int(choice) < 0 : 
+            raise ValueError 
+    except (ValueError, IndexError):
+        pass
+    return choice
 
-def display_sub_menu(menu_obj):
+def get_sub_menu_selection(menu_obj):
+    choice = input(">> ")
+    # check for errors
+    try:
+        if int(choice) < 0 : 
+            raise ValueError 
+        # Call the matching function
+        list(menu_obj[int(choice)].values())[0](menu_obj)
+
+    except (ValueError, IndexError):
+        pass
+    #return choice
+
+def display_menu_options(menu_obj):
     display_item = ''
     for item in menu_obj:
         display_item += colorize("[" + str(menu_obj.index(item)) + "] ", 'blue') + list(item.keys())[0] + " "
     print ( display_item )
 
-def get_selection(menu_obj):
-    choice = input(">> ")
-    # check for errors
-    try:
-        if int(choice) < 0 : raise ValueError
-        # Call the matching function
-        list(menu_obj[int(choice)].values())[0](menu_obj)
-    except (ValueError, IndexError):
-        pass
 
-
-def recursive_menu_selection(menu_obj):
-    display_menu(menu_obj)
-    get_selection(menu_obj)
+def next_menu_selection(menu_obj):
+    display_menu_options(menu_obj)
+    choice = get_main_menu_selection(menu_obj)
+    if choice == "0" :
+        print("In image menu selection")
+        display_menu_options(ann_image_menu_choices)
+        get_sub_menu_selection(ann_image_menu_choices)
+    elif choice == "1":
+        print("Here 2")
+    elif choice == "2":
+        print("Goodbye")
+        exit()
+    else:
+        next_menu_selection(menu_obj)
+'''
+    #select either image or container options
+    print(choice)
+    if choice == 0:
+        print ("choose image")
+        next_menu_selection(ann_image_menu_choices)
+        # choose image
+    elif choice == 1:
+        next_menu_selection(ann_container_menu_choices)
+        print ("choose container")
+        # choose container
+    else:
+        print("exiting")
+        next_menu_selection(menu_obj)
+'''
 
 def main_menu():
     os.system('clear')
     print(colorize(header, 'blue'))
-    display_sub_menu(ann_base_menu_choices)
-    get_selection(ann_base_menu_choices)
-    #recursive_menu_selection(ann_base_menu_choices)
+    next_menu_selection(ann_base_menu_choices)
 
+
+def back_to_base_menu(message = None):
+    main_menu()
+def exit_menu_loop(message = None):
+    exit()
     '''
     while True:
         os.system('clear')
@@ -124,19 +166,19 @@ ann_image_menu_choices = [
                                 {"Build Ann Image For Cpu": build_cpu_image},
                                 {"Build Ann Image For Gpu": build_gpu_image},
                                 {"Back To Main Menu": back_to_base_menu},
-                                {"Exit": exit}
+                                {"Exit": exit_menu_loop}
                          ]
 ann_container_menu_choices = [
-                                {"Enter Ann Container For Cpu Image": enter_cpu_image},
-                                {"Enter Ann Container For Gpu Image": enter_gpu_image},
+                                {"Enter Ann Cpu Container": enter_cpu_container},
+                                {"Enter Ann Gpu Container": enter_gpu_container},
                                 {"Back To Main Menu": back_to_base_menu},
-                                {"Exit": exit}
+                                {"Exit": exit_menu_loop}
                              ]
 # need two functions 
 ann_base_menu_choices = [
-                            {"Display Image Options": display_sub_menu},
-                            {"Display Container Options": display_sub_menu},
-                            {"Exit": exit}
+                            {"Display Image Options": display_menu_options},
+                            {"Display Container Options": display_menu_options},
+                            {"Exit": exit_menu_loop}
                         ]
 
 if __name__ == "__main__":
