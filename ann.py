b96c817f7066eb88072bf82c9adc9345f1e58d4a
zinhart <azdegrac@ucsc.edu>
2018-07-04 17:36:49 -0700

    Added additional menu options for clearing screen and listing containers and images. The output for listing containers and images needs to be fixed. Then docker-pty for terminal inside the container and this script is done

--- ann.py
+++ ann.py
@@ -1,4 +1,4 @@
-import os, sys, subprocess, docker, re, json
+import os, sys, subprocess, docker, dockerpty, re, json
 client = docker.from_env()
 header = "\
  .----------------. .----------------. .-----------------..----------------. .----------------. .----------------. .----------------.\n\
@@ -45,10 +45,14 @@ colors = {
 def colorize(string, color):
     if not color in colors: return string
     return colors[color] + string + '\033[0m'
-ann_image_build_cmds = [
-                        "docker build",
-                        "nvidia-docker build -t ann ."
-                       ]
+
+def clear_screen(menu_obj = None):
+    os.system('clear')
+    if menu_obj != None:
+        display_menu_options(menu_obj)
+        get_sub_menu_selection(menu_obj)
+
+
 def build_cpu_image(menu_obj):
     output_bytes = client.build(path = "./", tag = "ann")
     for item in output_bytes:
@@ -57,23 +61,26 @@ def build_cpu_image(menu_obj):
         print(pretty_output, end='')
     display_menu_options(menu_obj)
     get_sub_menu_selection(menu_obj)
-    #next_menu_selection(menu_obj)
     #input("Press [Enter] to continue")
 
 def enter_cpu_container(menu_obj):
     print("In enter cpu container")
+    #dockerpty
     return;
 
 #have to set this to use nvidia-docker via the client runtime parameter (something like that)
-def build_gpu_image():
+def build_gpu_image(menu_obj):
     output_bytes = client.build(path = "./", tag = "ann")
     for item in output_bytes:
         pretty_output = json.loads(item)
         pretty_output = pretty_output['stream']
         print(pretty_output, end='')
-    input("Press [Enter] to continue")
+    display_menu_options(menu_obj)
+    get_sub_menu_selection(menu_obj)
+    #input("Press [Enter] to continue")
 
 def enter_gpu_container():
+    #dockerpty
     return;
 
 
@@ -95,10 +102,8 @@ def get_sub_menu_selection(menu_obj):
             raise ValueError 
         # Call the matching function
         list(menu_obj[int(choice)].values())[0](menu_obj)
-
     except (ValueError, IndexError):
         pass
-    #return choice
 
 def display_menu_options(menu_obj):
     display_item = ''
@@ -106,69 +111,60 @@ def display_menu_options(menu_obj):
         display_item += colorize("[" + str(menu_obj.index(item)) + "] ", 'blue') + list(item.keys())[0] + " "
     print ( display_item )
 
-
+'''
+Recursively loops through menu options
+'''
 def next_menu_selection(menu_obj):
     display_menu_options(menu_obj)
     choice = get_main_menu_selection(menu_obj)
-    if choice == "0" :
+    
+    if choice == str(len(menu_obj)-1):# always check for exit condition first
+        exit_menu_loop()
+    elif choice == "0" :
         print("In image menu selection")
         display_menu_options(ann_image_menu_choices)
         get_sub_menu_selection(ann_image_menu_choices)
     elif choice == "1":
+        display_menu_options(ann_container_menu_choices)
+        get_sub_menu_selection(ann_container_menu_choices)
         print("Here 2")
-    elif choice == "2":
-        print("Goodbye")
-        exit()
     else:
         next_menu_selection(menu_obj)
-'''
-    #select either image or container options
-    print(choice)
-    if choice == 0:
-        print ("choose image")
-        next_menu_selection(ann_image_menu_choices)
-        # choose image
-    elif choice == 1:
-        next_menu_selection(ann_container_menu_choices)
-        print ("choose container")
-        # choose container
-    else:
-        print("exiting")
-        next_menu_selection(menu_obj)
-'''
 
 def main_menu():
-    os.system('clear')
+    clear_screen()
     print(colorize(header, 'blue'))
     next_menu_selection(ann_base_menu_choices)
 
-
 def back_to_base_menu(message = None):
     main_menu()
+
 def exit_menu_loop(message = None):
     exit()
-    '''
-    while True:
-        os.system('clear')
-        display_item = ''
-        for item in ann_image_menu_choices:
-            display_item += colorize("[" + str(ann_image_menu_choices.index(item)) + "] ", 'blue') + list(item.keys())[0] + " "
-        print ( display_item )
-        choice = input(">> ")
-        try:
-            if int(choice) < 0 : raise ValueError
-            # Call the matching function
-            list(ann_image_menu_choices[int(choice)].values())[0]()
-        except (ValueError, IndexError):
-            pass
-       '''     
+
+def list_images(menu_obj):
+    print (client.images(all=True))
+    display_menu_options(menu_obj)
+    get_sub_menu_selection(menu_obj)
+
+def list_containers(menu_obj):
+    print (client.containers(all=True))
+    display_menu_options(menu_obj)
+    get_sub_menu_selection(menu_obj)
+
+
+
 ann_image_menu_choices = [
+                                {"List Images": list_images},
+                                {"Clear": clear_screen},
                                 {"Build Ann Image For Cpu": build_cpu_image},
                                 {"Build Ann Image For Gpu": build_gpu_image},
                                 {"Back To Main Menu": back_to_base_menu},
                                 {"Exit": exit_menu_loop}
                          ]
 ann_container_menu_choices = [
+                                {"List Containers": list_containers},
+                                {"Clear": clear_screen},
                                 {"Enter Ann Cpu Container": enter_cpu_container},
                                 {"Enter Ann Gpu Container": enter_gpu_container},
                                 {"Back To Main Menu": back_to_base_menu},
