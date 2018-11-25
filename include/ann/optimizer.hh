727a478b329586fd240ce117b210f956dad42777
zinhart <aromzinhartdegraca@gmail.com>
2018-11-24 23:16:28 -0800

    minor optimizer refactor

--- include/ann/optimizer.hh
+++ include/ann/optimizer.hh
@@ -118,6 +118,16 @@ namespace zinhart
 	template <class precision_type>
 	  class optimizer
 	  {
+		public:
+  		  HOST void update(precision_type * weights, const precision_type * const gradient, const std::uint32_t length, const std::uint32_t n_threads = 1, const std::uint32_t thread_id = 0);
+		  HOST void set_size(const std::uint32_t size);
+		  HOST std::uint32_t get_size()const;
+		  HOST void update_bias_correction();
+		  HOST precision_type get_bias_corrected_first_moment()const;
+		  HOST precision_type get_bias_corrected_second_moment()const;
+		  HOST void safe_deallocate();
+		  HOST std::string name()const;
+		  HOST virtual ~optimizer();
 		protected:
 		  optimum<precision_type> opt;
 		  std::uint32_t size;
@@ -129,21 +139,18 @@ namespace zinhart
 		  HOST virtual precision_type get_bias_corrected_second_moment_impl()const{ return 0; };
 		  HOST virtual void safe_deallocate_impl(){};
 		  HOST virtual std::string name_impl()const=0;
-		public:
-  		  HOST void update(precision_type * weights, const precision_type * const gradient, const std::uint32_t length, const std::uint32_t n_threads = 1, const std::uint32_t thread_id = 0);
-		  HOST void set_size(const std::uint32_t size);
-		  HOST std::uint32_t get_size()const;
-		  HOST void update_bias_correction();
-		  HOST precision_type get_bias_corrected_first_moment()const;
-		  HOST precision_type get_bias_corrected_second_moment()const;
-		  HOST void safe_deallocate();
-		  HOST std::string name()const;
-		  HOST virtual ~optimizer();
 	  };
 
 	template <class precision_type>
   	  class sgd : public optimizer<precision_type>
 	  {
+		public:
+		  HOST sgd(precision_type learning_rate = 0.01);
+		  HOST sgd(const sgd&) = delete;
+		  HOST sgd(sgd &&) = delete;
+		  HOST sgd & operator = (const sgd&) = delete;
+		  HOST sgd & operator = (sgd &&) = delete;
+		  HOST ~sgd() = default;
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -152,20 +159,19 @@ namespace zinhart
 		  HOST virtual void set_size_impl(const std::uint32_t & size) override;
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST sgd(precision_type learning_rate = 0.01);
-		  HOST sgd(const sgd&) = delete;
-		  HOST sgd(sgd &&) = delete;
-		  HOST sgd & operator = (const sgd&) = delete;
-		  HOST sgd & operator = (sgd &&) = delete;
-		  HOST ~sgd() = default;
 	  };
 
 
 	template <class precision_type>
   	  class momentum : public optimizer<precision_type>
 	  {
-
+		public:
+		  HOST momentum(std::uint32_t size, precision_type learning_rate = 0.01, precision_type momentum_term = 0.9);
+		  HOST momentum(const momentum & m) = delete;
+		  HOST momentum(momentum && m) = delete;
+		  HOST momentum & operator = (const momentum & m) = delete;
+		  HOST momentum & operator = (momentum && m) = delete;
+		  HOST ~momentum();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -177,18 +183,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST momentum(std::uint32_t size, precision_type learning_rate = 0.01, precision_type momentum_term = 0.9);
-		  HOST momentum(const momentum & m) = delete;
-		  HOST momentum(momentum && m) = delete;
-		  HOST momentum & operator = (const momentum & m) = delete;
-		  HOST momentum & operator = (momentum && m) = delete;
-		  HOST ~momentum();
 	  };
 
 	template <class precision_type>
   	  class nesterov_momentum : public optimizer<precision_type>
 	  {
+		public:
+		  HOST nesterov_momentum(std::uint32_t size, precision_type learning_rate = 0.01, precision_type momentum_term = 0.9);
+		  HOST nesterov_momentum(const nesterov_momentum & nm) = delete;
+		  HOST nesterov_momentum(nesterov_momentum && nm) = delete;
+		  HOST nesterov_momentum & operator = (const nesterov_momentum & nm) = delete;
+		  HOST nesterov_momentum & operator = (nesterov_momentum && nm) = delete;
+		  HOST ~nesterov_momentum();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -200,18 +206,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST nesterov_momentum(std::uint32_t size, precision_type learning_rate = 0.01, precision_type momentum_term = 0.9);
-		  HOST nesterov_momentum(const nesterov_momentum & nm) = delete;
-		  HOST nesterov_momentum(nesterov_momentum && nm) = delete;
-		  HOST nesterov_momentum & operator = (const nesterov_momentum & nm) = delete;
-		  HOST nesterov_momentum & operator = (nesterov_momentum && nm) = delete;
-		  HOST ~nesterov_momentum();
 	  };
 
 	template <class precision_type>
   	  class adagrad : public optimizer<precision_type>
 	  {
+		public:
+		  HOST adagrad(std::uint32_t size, precision_type learning_rate = 0.01, precision_type epsilon = 1.e-8);
+		  HOST adagrad(const adagrad & a) = delete;
+		  HOST adagrad(adagrad && a) = delete;
+		  HOST adagrad & operator = (const adagrad & a) = delete;
+		  HOST adagrad & operator = (adagrad && a) = delete;
+		  HOST ~adagrad();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -223,19 +229,19 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST adagrad(std::uint32_t size, precision_type learning_rate = 0.01, precision_type epsilon = 1.e-8);
-		  HOST adagrad(const adagrad & a) = delete;
-		  HOST adagrad(adagrad && a) = delete;
-		  HOST adagrad & operator = (const adagrad & a) = delete;
-		  HOST adagrad & operator = (adagrad && a) = delete;
-		  HOST ~adagrad();
 	  };
 
 
 	template <class precision_type>
   	  class conjugate_gradient : public optimizer<precision_type>
 	  {
+		public:
+		  HOST conjugate_gradient(std::uint32_t size, precision_type epsilon = 1.e-30);
+		  conjugate_gradient(const conjugate_gradient&) = delete;
+		  conjugate_gradient(conjugate_gradient &&) = delete;
+		  conjugate_gradient & operator = (const conjugate_gradient&) = delete;
+		  conjugate_gradient & operator = (conjugate_gradient &&) = delete;
+		  HOST ~conjugate_gradient();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -247,18 +253,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST conjugate_gradient(std::uint32_t size, precision_type epsilon = 1.e-30);
-		  conjugate_gradient(const conjugate_gradient&) = delete;
-		  conjugate_gradient(conjugate_gradient &&) = delete;
-		  conjugate_gradient & operator = (const conjugate_gradient&) = delete;
-		  conjugate_gradient & operator = (conjugate_gradient &&) = delete;
-		  HOST ~conjugate_gradient();
 	  };
 
 	template <class precision_type>
   	  class adadelta : public optimizer<precision_type>
 	  {
+		public:
+		  HOST adadelta(std::uint32_t size, precision_type gamma = 0.99, precision_type epsilon = 1.e-6 );
+		  adadelta(const adadelta&) = delete;
+		  adadelta(adadelta &&) = delete;
+		  adadelta & operator = (const adadelta&) = delete;
+		  adadelta & operator = (adadelta &&) = delete;
+		  HOST ~adadelta();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -271,18 +277,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST adadelta(std::uint32_t size, precision_type gamma = 0.99, precision_type epsilon = 1.e-6 );
-		  adadelta(const adadelta&) = delete;
-		  adadelta(adadelta &&) = delete;
-		  adadelta & operator = (const adadelta&) = delete;
-		  adadelta & operator = (adadelta &&) = delete;
-		  HOST ~adadelta();
 	  };
 
 	template <class precision_type>
   	  class rms_prop : public optimizer<precision_type>
 	  {
+		public:
+		  HOST rms_prop(std::uint32_t size, precision_type learning_rate = 0.001, precision_type gamma = 0.90, precision_type epsilon = 1.e-8);
+		  rms_prop(const rms_prop&) = delete;
+		  rms_prop(rms_prop &&) = delete;
+		  rms_prop & operator = (const rms_prop&) = delete;
+		  rms_prop & operator = (rms_prop &&) = delete;
+		  HOST ~rms_prop();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -295,18 +301,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST rms_prop(std::uint32_t size, precision_type learning_rate = 0.001, precision_type gamma = 0.90, precision_type epsilon = 1.e-8);
-		  rms_prop(const rms_prop&) = delete;
-		  rms_prop(rms_prop &&) = delete;
-		  rms_prop & operator = (const rms_prop&) = delete;
-		  rms_prop & operator = (rms_prop &&) = delete;
-		  HOST ~rms_prop();
 	  };
 
 	template <class precision_type>
   	  class rprop : public optimizer<precision_type>
 	  {
+		public:
+		  HOST rprop(std::uint32_t size, precision_type learning_rate_pos = 1.2, precision_type learning_rate_neg = 0.5, precision_type delta_max = 50, precision_type delta_min = 1.e-6);
+		  rprop(const rprop&) = delete;
+		  rprop(rprop &&) = delete;
+		  rprop & operator = (const rprop&) = delete;
+		  rprop & operator = (rprop &&) = delete;
+		  HOST ~rprop();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -321,18 +327,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST rprop(std::uint32_t size, precision_type learning_rate_pos = 1.2, precision_type learning_rate_neg = 0.5, precision_type delta_max = 50, precision_type delta_min = 1.e-6);
-		  rprop(const rprop&) = delete;
-		  rprop(rprop &&) = delete;
-		  rprop & operator = (const rprop&) = delete;
-		  rprop & operator = (rprop &&) = delete;
-		  HOST ~rprop();
 	  };
 
 	template <class precision_type>
   	  class adamax : public optimizer<precision_type>
 	  {
+		public:
+		  HOST adamax(std::uint32_t size, precision_type learning_rate = 0.002, precision_type beta_1 = 0.9, precision_type beta_2 = 0.999, precision_type beta_1_t = 0.9, precision_type epsilon = 1e-8);
+		  adamax(const adamax&) = delete;
+		  adamax(adamax &&) = delete;
+		  adamax & operator = (const adamax&) = delete;
+		  adamax & operator = (adamax &&) = delete;
+		  HOST ~adamax();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -350,19 +356,19 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST adamax(std::uint32_t size, precision_type learning_rate = 0.002, precision_type beta_1 = 0.9, precision_type beta_2 = 0.999, precision_type beta_1_t = 0.9, precision_type epsilon = 1e-8);
-		  adamax(const adamax&) = delete;
-		  adamax(adamax &&) = delete;
-		  adamax & operator = (const adamax&) = delete;
-		  adamax & operator = (adamax &&) = delete;
-		  HOST ~adamax();
 	  };
 
 
 	template <class precision_type>
   	  class amsgrad : public optimizer<precision_type>
 	  {
+		public:
+		  HOST amsgrad(std::uint32_t size, precision_type learning_rate = 0.01, precision_type beta_1 = 0.9, precision_type beta_2 = 0.99, precision_type epsilon = 1.e-8);
+		  amsgrad(const amsgrad&) = delete;
+		  amsgrad(amsgrad &&) = delete;
+		  amsgrad & operator = (const amsgrad&) = delete;
+		  amsgrad & operator = (amsgrad &&) = delete;
+		  HOST ~amsgrad();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -378,19 +384,18 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST amsgrad(std::uint32_t size, precision_type learning_rate = 0.01, precision_type beta_1 = 0.9, precision_type beta_2 = 0.99, precision_type epsilon = 1.e-8);
-		  amsgrad(const amsgrad&) = delete;
-		  amsgrad(amsgrad &&) = delete;
-		  amsgrad & operator = (const amsgrad&) = delete;
-		  amsgrad & operator = (amsgrad &&) = delete;
-		  HOST ~amsgrad();
 	  };
 
 	template <class precision_type>
   	  class adam : public optimizer<precision_type>
 	  {
-		
+		public:
+		  HOST adam(std::uint32_t size, precision_type learning_rate = 0.001, precision_type beta_1 = 0.9, precision_type beta_2 = 0.999, precision_type beta_1_t = 0.9, precision_type beta_2_t = 0.999, precision_type epsilon = 1.e-8);
+		  adam(const adam&) = delete;
+		  adam(adam &&) = delete;
+		  adam & operator = (const adam&) = delete;
+		  adam & operator = (adam &&) = delete;
+		  HOST ~adam();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -410,19 +415,19 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST adam(std::uint32_t size, precision_type learning_rate = 0.001, precision_type beta_1 = 0.9, precision_type beta_2 = 0.999, precision_type beta_1_t = 0.9, precision_type beta_2_t = 0.999, precision_type epsilon = 1.e-8);
-		  adam(const adam&) = delete;
-		  adam(adam &&) = delete;
-		  adam & operator = (const adam&) = delete;
-		  adam & operator = (adam &&) = delete;
-		  HOST ~adam();
 	  };
 
 
 	template <class precision_type>
   	  class nadam : public optimizer<precision_type>
 	  {
+		public:
+		  HOST nadam(std::uint32_t size, precision_type learning_rate = 0.001, precision_type gamma = 0.9, precision_type beta_1 = 0.9, precision_type beta_2 = 0.999, precision_type beta_1_t = 0.9, precision_type beta_2_t = 0.999, precision_type epsilon = 1.e-8);
+		  nadam(const nadam&) = delete;
+		  nadam(nadam &&) = delete;
+		  nadam & operator = (const nadam&) = delete;
+		  nadam & operator = (nadam &&) = delete;
+		  HOST ~nadam();
 		private:
 		  using optimizer<precision_type>::opt; 
 		  using optimizer<precision_type>::size;
@@ -443,13 +448,6 @@ namespace zinhart
 		  HOST virtual std::uint32_t get_size_impl()const override;
 		  HOST void safe_deallocate_impl()override;
 		  HOST virtual std::string name_impl()const override;
-		public:
-		  HOST nadam(std::uint32_t size, precision_type learning_rate = 0.001, precision_type gamma = 0.9, precision_type beta_1 = 0.9, precision_type beta_2 = 0.999, precision_type beta_1_t = 0.9, precision_type beta_2_t = 0.999, precision_type epsilon = 1.e-8);
-		  nadam(const nadam&) = delete;
-		  nadam(nadam &&) = delete;
-		  nadam & operator = (const nadam&) = delete;
-		  nadam & operator = (nadam &&) = delete;
-		  HOST ~nadam();
 	  };
 #if CUDA_ENABLED == 1
 	void optimize(optimizer<double> * const o, double * weights, const double * const gradient, const std::uint32_t & length);
